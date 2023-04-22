5b03637765e8c9b73f4e1d06ccc6d0d40e4a5ca5
Mikayil Aktemur <mikayil34@gmail.com>
2023-04-22 05:49:20 +0300

    add

---
+++ XSD_Document/XSD_Document/Dto/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+
+// Örnek bir XSD belgesi tanımı
+[XmlRoot("Person")]
+public class Person
+{
+    [XmlAttribute("ID")]
+    public string ID { get; set; }
+
+    [XmlElement("FirstName")]
+    public string FirstName { get; set; }
+
+    [XmlElement("LastName")]
+    public string LastName { get; set; }
+
+    [XmlElement("Email")]
+    public string Email { get; set; }
+
+    [XmlElement("Address")]
+    public string Address { get; set; }
+}
+
+ 
---
+++ XSD_Document/XSD_Document/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using XSD_Document.Dto;
+
+namespace XSD_Document
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string xsdPath = @"D:\\user\\example.xsd";
+             
+
+            // XSD belgesi için örnek bir nesne oluşturuluyor
+            Person person = new Person()
+            {
+                ID = "1",
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "johndoe@example.com",
+                Address = "123 Main St"
+            };
+
+            // Nesne, XSD belgesine serileştiriliyor
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            using (TextWriter writer = new StreamWriter(xsdPath))
+            {
+                serializer.Serialize(writer, person);
+            }
+
+            Console.WriteLine("XSD belgesi oluşturuldu!");
+
+        }
+    }
+}
